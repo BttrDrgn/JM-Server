a801df8437ec8afe22213b4f245f41dc26eef8bd
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2022-08-21 14:20:39 -0500

    Update start.bat for pm2 usage

--- start.bat
+++ start.bat
@@ -1,2 +1,9 @@
+@echo off
+
+rem Start MonogDB if not already started
 net start MongoDB
-node app.js
+
+rem npx runs packages if they exists and installs and runs if they do not exist
+rem pm2 info will return info if the process is running
+rem if not running, start, else just print info
+npx pm2 info jm-server || npx pm2 start app.js --name jm-server
